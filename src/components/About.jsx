d7b46ee20eab2d72e1ec897a6c29f32149e69bd7
Aroon <aroonsalotra@gmail.com>
2026-04-25 15:08:02 +0100

    add clickable redirects to about section with animation for copying to clipboard

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,60 @@
 import headshot from "../assets/headshot.jpg"
 import { FaLinkedin, FaPhone } from "react-icons/fa";
 import { AiFillMail } from "react-icons/ai";
+import { useState } from "react";
 
 const About = ({ ref }) => {
+
+    const [showMsg, setShowMsg] = useState(null);
+
+    const handleClick = (target) => {
+
+        let copyText = null;
+        if (target == "phone") {
+            copyText = "07397377081"
+        }
+
+        if (target == "email") {
+            copyText = "aroonsalotra@gmail.com"
+        }
+
+        navigator.clipboard.writeText(copyText);
+
+        setShowMsg(true);
+        setTimeout(() => {
+            setShowMsg(false);
+        }, 1200);
+    }
+
     return (
         <div ref={ref} className="container-about">
             <div className="content-about">
-                <h2 style={{border: "none"}}>Aroon Salotra</h2>
+                <h2 style={{ border: "none" }}>Aroon Salotra</h2>
                 <h3>Full Stack Developer</h3>
 
                 <img className="img-about" src={headshot} />
 
                 <ul className="about-icons">
-                    <li><FaLinkedin className="icon-s" /></li>
-                    <li><FaPhone className="icon-s" /></li>
-                    <li><AiFillMail className="icon-s" /></li>
+                    <li>
+                        <a target="_blank" href="https://www.linkedin.com/in/aroon-salotra/">
+                            <FaLinkedin className="icon-s clickable" />
+                        </a>
+                    </li>
+                    <li>
+                        <FaPhone onClick={() => handleClick("phone")} className="icon-s clickable" />
+                    </li>
+                    <li>
+                        <AiFillMail onClick={() => handleClick("email")} className="icon-s clickable" />
+                    </li>
                 </ul>
 
-                <button className="btn-about">
+                <p className={`msg ${showMsg ? 'show' : 'hide'}`}>
+                    Copied to clipboard
+                </p>
+
+                <a target="_blank" href="https://drive.google.com/file/d/1mhXvuFkDhHA70f8cuKfPf08738qbkrGA/view?usp=sharing" className="btn-about">
                     Resume
-                </button>
+                </a>
             </div>
         </div>
     );
